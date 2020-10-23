6e5d544043c0524c89698cf02d01c4a3084f5764
MengQiu_7723 <505772824@qq.com>
2020-10-23 10:44:13 +0800

    管理员功能

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,112 @@
 <template>
   <div>
-    <h3>欢迎来到35后台管理系统</h3>
+    <Head></Head>
+
+    <div>
+      <el-steps :active="active" finish-status="success">
+        <el-step title="步骤 1"></el-step>
+        <el-step title="步骤 2"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </div>
+    <div style="margin: 20px"></div>
+    <el-form
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formLabelAlign"
+    >
+      <div v-if="active == 0">
+        <p>第一个表单</p>
+        <el-form-item label="名称">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+      </div>
+
+      <div v-if="active == 1">
+        <p>第二个表单</p>
+        <el-form-item label="名称2">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域2">
+          <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式2">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+      </div>
+
+      <div v-if="active == 2">
+        <p>最后一个表单</p>
+        <el-form-item label="名称3">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域3">
+          <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式3">
+          <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+      </div>
+    </el-form>
+
+    <el-button style="margin-top: 12px" @click="next" v-if="active < 3">{{
+      messageTip
+    }}</el-button>
+    <el-button
+      style="margin-top: 12px"
+      @click="pre"
+      v-if="active > 0 && active <= 2"
+      >上一步</el-button
+    >
   </div>
 </template>
+
+
+<script>
+import Head from '../components/Head'
+export default {
+  data() {
+    return {
+      active: 0,
+      messageTip: '下一步',
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: '',
+      },
+    }
+  },
+  components: {
+    Head,
+  },
+  methods: {
+    next() {
+      if (this.active++ > 2) {
+        this.active = 0
+      }
+      if (this.active == 2) {
+        this.messageTip = '完成'
+      } else {
+        this.messageTip = '下一步'
+      }
+    },
+    pre() {
+      if (this.active-- < 1) {
+        this.active = 1
+      }
+      if (this.active == 2) {
+        this.messageTip = '完成'
+      } else {
+        this.messageTip = '下一步'
+      }
+    },
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -8,20 +8,30 @@ import './plugins/element.js'
 import 'element-ui/lib/theme-chalk/index.css'
 // import './element-variables.scss'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 导入全局样式表
 import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
 // 配置请求的根路径
-// axios.defaults.baseURL = 'http://192.168.1.103:8080'
+axios.defaults.baseURL = '/api/'
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 // 配置axios请求拦截器
-axios.interceptors.request.use((config) => {
-  // console.log(config)
-  config.headers.Authorization = window.sessionStorage.getItem('token');
-  return config
-});
+// axios.interceptors.request.use((config) => {
+//   // console.log(config)
+//   config.headers.Authorization = window.sessionStorage.getItem('token');
+//   return config
+// });
 Vue.prototype.$http = axios
 
 new Vue({
