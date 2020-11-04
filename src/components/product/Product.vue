ae5e0372f7907c6a2ebe83d4a29a7f909bf0ea80
Answers886 <1347105141@qq.com>
2020-11-04 20:03:37 +0800

    书本信息修改和新增

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -24,34 +24,25 @@
       <!--商品列表区域-->
       <el-table :data="bookList" border stripe>
         <el-table-column label="书本名称" prop="bookName"></el-table-column>
-        <el-table-column label="书本类型" prop="booktype"></el-table-column>
-        <el-table-column label="上架时间" prop="booktime"></el-table-column>
-        <el-table-column label="价格" prop="money"></el-table-column>
+        <el-table-column label="出版社" prop="publisher"></el-table-column>
+        <el-table-column label="作者" prop="author"></el-table-column>
+        <el-table-column label="价格" prop="price"></el-table-column>
+        <el-table-column label="内容介绍" prop="introduce"></el-table-column>
+        <el-table-column label="书本编号" prop="ISBN"></el-table-column>
+        <el-table-column label="书本封面" prop="imagesUrl"></el-table-column>
         <el-table-column label="操作">
-          <template>
+          <template slot-scope="scope">
             <el-button
               type="primary"
               icon="el-icon-edit"
               size="mini"
+              @click="showEditDialog(scope.row.id)"
             ></el-button>
             <el-button
               type="danger"
               icon="el-icon-delete"
               size="mini"
             ></el-button>
-
-            <el-tooltip
-              effect="dark"
-              content="分配用户"
-              placement="top"
-              :enterable="false"
-            >
-              <el-button
-                type="warning"
-                icon="el-icon-setting"
-                size="mini"
-              ></el-button>
-            </el-tooltip>
           </template>
         </el-table-column>
       </el-table>
@@ -83,17 +74,26 @@
         ref="addFormRef"
         label-width="70px"
       >
-        <el-form-item label="书本名称" prop="bookname">
-          <el-input v-model="addForm.bookname"></el-input>
+        <el-form-item label="书本名称" prop="bookName">
+          <el-input v-model="addForm.bookName"></el-input>
         </el-form-item>
-        <el-form-item label="书本类型" prop="booktype">
-          <el-input v-model="addForm.password"></el-input>
+        <el-form-item label="出版社" prop="publisher">
+          <el-input v-model="addForm.publisher"></el-input>
         </el-form-item>
-        <el-form-item label="上架时间" prop="booktime">
-          <el-input v-model="addForm.booktime"></el-input>
+        <el-form-item label="作者" prop="author">
+          <el-input v-model="addForm.author"></el-input>
         </el-form-item>
-        <el-form-item label="价格" prop="money">
-          <el-input v-model="addForm.money"></el-input>
+        <el-form-item label="价格" prop="price">
+          <el-input v-model="addForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="内容介绍" prop="introduce">
+          <el-input v-model="addForm.introduce"></el-input>
+        </el-form-item>
+        <el-form-item label="书本编号" prop="ISBN">
+          <el-input v-model="addForm.ISBN"></el-input>
+        </el-form-item>
+        <el-form-item label="书本封面" prop="imagesUrl">
+          <el-input v-model="addForm.imagesUrl"></el-input>
         </el-form-item>
       </el-form>
       <!-- 底部区域 -->
@@ -102,6 +102,46 @@
         <el-button type="primary" @click="addDialogVisible = false"
           >确 定</el-button
         >
+      </span>
+    </el-dialog>
+    <!-- 修改用户的对话框 -->
+    <el-dialog
+      title="修改用户"
+      :visible.sync="editDialogVisible"
+      width="50%"
+      @close="editDialogClosed"
+    >
+      <el-form
+        :model="editForm"
+        :rules="editFormRules"
+        ref="editFormRef"
+        label-width="70px"
+      >
+        <el-form-item label="书本名称">
+          <el-input v-model="editForm.bookName"></el-input>
+        </el-form-item>
+        <el-form-item label="出版社">
+          <el-input v-model="editForm.publisher"></el-input>
+        </el-form-item>
+        <el-form-item label="作者">
+          <el-input v-model="editForm.author"></el-input>
+        </el-form-item>
+        <el-form-item label="价格">
+          <el-input v-model="editForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="内容介绍">
+          <el-input v-model="editForm.introduce"></el-input>
+        </el-form-item>
+        <el-form-item label="书本编号">
+          <el-input v-model="editForm.ISBN"></el-input>
+        </el-form-item>
+        <el-form-item label="书本封面">
+          <el-input v-model="editForm.imagesUrl"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -118,12 +158,13 @@ export default {
       },
       total: 0,
       bookList: [
-        {
-          bookName: "唐诗三百首",
-          booktype: "文学",
-          booktime: "2020.1.23",
-          money: "9.9",
-        },
+        // {
+        //   id: 1,
+        //   bookName: "唐诗三百首",
+        //   booktype: "文学",
+        //   booktime: "2020.1.23",
+        //   money: "9.9",
+        // },
       ],
       //控制对话框的显示与隐藏
       addDialogVisible: false,
@@ -136,7 +177,7 @@ export default {
       },
       // 添加表单的验证规则对象
       addFormRules: {
-        bookname: [
+        /* bookname: [
           { required: true, message: "请输入用户名", trigger: "blur" },
           {
             min: 3,
@@ -152,8 +193,8 @@ export default {
             max: 15,
             message: "用户名的长度在6~15个字符之间",
             trigger: "blur",
-          },
-        ],
+          }, 
+         ],
         booktime: [
           { required: true, message: "请输入时间", trigger: "blur" },
           // { validator: checkEmail, trigger: "blur" },
@@ -161,10 +202,31 @@ export default {
         money: [
           { required: true, message: "请输入价格", trigger: "blur" },
           // { validator: checkMobile, trigger: "blur" },
-        ],
+        ], */
       },
+
       // 控制修改用户对话框的显示与隐藏
       editDialogVisible: false,
+      editForm: {
+        id: "int",
+        cid: "int //分类id",
+        sid: "int //店铺名id",
+        bookName: "string //书名",
+        publisher: "string //出版社",
+        author: "string //作者名称",
+        price: "float //价格",
+        introduce: "string //内容介绍",
+        ISBN: "string //书本编号",
+        imagesUrl: "string //书本封面（图片）",
+        modifyCategory: "string //暂时用不到",
+        ggct: "string //暂时用不到",
+        returnGoods: "string //暂时用不到",
+        invoice: "string //暂时用不到",
+        promise: "string //暂时用不到",
+        region: "string //发货地",
+        specialOffer: "string //特价",
+      },
+      editFormRules: {},
     };
   },
 
@@ -173,7 +235,7 @@ export default {
   },
   methods: {
     async getGoodsList() {
-      const { data: res } = await this.$http.get("/api/book/findAll ");
+      const { data: res } = await this.$http.get("/book/findAll");
       this.bookList = res.data;
       console.log(res.code);
     },
@@ -208,6 +270,43 @@ export default {
         this.addDialogVisible = false;
         // 重新获取用户列表数据
         this.getUserList();
+      });
+    },
+    async showEditDialog(id) {
+      // console.log(id)
+      const { data: res } = await this.$http.get("/book/getById", {
+        params: { id: id },
+      });
+      console.log(res);
+      if (res.code !== 0) {
+        return this.$message.error("查询用户信息失败！");
+      }
+      this.editForm = res.data;
+      this.editDialogVisible = true;
+    },
+    // 监听修改用户对话框的关闭事件
+    editDialogClosed() {
+      this.$refs.editFormRef.resetFields();
+    },
+    // 修改用户信息并提交
+    editUserInfo() {
+      this.$refs.editFormRef.validate(async (valid) => {
+        if (!valid) return;
+        // 发起修改用户信息的数据请求
+        const { data: res } = await this.$http.post(
+          "/book/update",
+          this.editForm
+        );
+        console.log(res);
+        if (res.code !== 0) {
+          return this.$message.error("更新用户信息失败！");
+        }
+        // 关闭对话框
+        this.editDialogVisible = false;
+        // 刷新数据列表
+        this.getGoodsList();
+        // 提示修改成功
+        this.$message.success("更新用户信息成功！");
       });
     },
     handleClose() {},
