5285c175cf5c19201b678ae374021e7871bfde58
Answers886 <1347105141@qq.com>
2020-11-05 11:29:43 +0800

    修改

--- src/components/product/Product02.vue
+++ src/components/product/Product02.vue
@@ -4,7 +4,7 @@
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
-      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
       <div class="block">
@@ -63,9 +63,7 @@ export default {
     this.getBookTreeList();
   },
   methods: {
-    iseEdit(){
-      
-    },
+    iseEdit() {},
     handleNodeClick(data) {
       console.log(data);
     },
